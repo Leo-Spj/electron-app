b321c9309218956d6c41b7091831a7340169f822
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2023-10-24 23:49:21 -0500

    maneno de barra de menus, labels accelerator y clicks

--- src/main.js
+++ src/main.js
@@ -1,12 +1,42 @@
 // todo lo relacionado con electron
 
-const {BrowserWindow} = require('electron');
+const {BrowserWindow, Menu, app} = require('electron');
 
 function createWindow () {
   
     const mainWindow = new BrowserWindow({ width: 800, height: 700 });
+    const mainMenu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(mainMenu);
 
     mainWindow.loadFile('src/gui/html/index.html');
 }
+
+//Custom main menu
+const template = [
+    {
+        label: 'Inicio',
+        submenu: [
+            {
+                label: 'Exit',
+                accelerator: 'Ctrl+Q',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    },
+    {
+        label: 'Herramientas',
+        submenu: [
+            {
+                label: 'DevTools',
+                accelerator: 'Ctrl+D',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            }
+        ]
+    }
+];
 
 module.exports = {createWindow};
